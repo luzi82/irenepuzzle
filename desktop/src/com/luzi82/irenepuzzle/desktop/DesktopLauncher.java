b1ec631212efd58dda3d0677c478e523d4c7b140
Luzi82 <luzi82@gmail.com>
2018-11-22 22:54:26 +0800

    init project

---
+++ desktop/src/com/luzi82/irenepuzzle/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.luzi82.irenepuzzle.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.luzi82.irenepuzzle.IrenePuzzleGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new IrenePuzzleGame(), config);
+	}
+}
