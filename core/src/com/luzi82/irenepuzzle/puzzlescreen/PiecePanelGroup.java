c29544656efb4de9d613873c21f36426cc8d6eca
Luzi82 <luzi82@gmail.com>
2018-11-25 23:21:22 +0800

    remove comment

--- core/src/com/luzi82/irenepuzzle/puzzlescreen/PiecePanelGroup.java
+++ core/src/com/luzi82/irenepuzzle/puzzlescreen/PiecePanelGroup.java
@@ -11,14 +11,12 @@ public class PiecePanelGroup extends Group {
 
     // const
     static public final float PIECE_PANEL_HEIGHT_RATIO = 8;
-//    static public final float[] PIECE_PANEL_WSEN = {0, 0, 1, PIECE_PANEL_HEIGHT_RATIO};
-//    static public final float[] PIECE_PANEL_WH = Utils.wsenToWh(PIECE_PANEL_WSEN);
     static public final Rectangle INNER_RECT = new Rectangle(0, 0, 1, PIECE_PANEL_HEIGHT_RATIO);
     static public final float GRAY = Utils.PHI - 1;
     static public final Color BG_COLOR = new Color(GRAY, GRAY, GRAY, 1f);
 
     static public final float PIECE_SIZE = 0.8f;
-    static public final float PIECE_OFFSET = (1-PIECE_SIZE)/2f;
+    static public final float PIECE_OFFSET = (1 - PIECE_SIZE) / 2f;
 
     // link
     final PuzzleScreen parent;
@@ -44,8 +42,8 @@ public class PiecePanelGroup extends Group {
         pieceImageAry = new Image[parent.pieceTextureRegionAry.length];
         for (int i = 0; i < pieceImageAry.length; ++i) {
             pieceImageAry[i] = new Image(parent.pieceTextureRegionAry[i]);
-            pieceImageAry[i].setSize(PIECE_SIZE,PIECE_SIZE);
-            pieceImageAry[i].setPosition(PIECE_OFFSET,i+PIECE_OFFSET);
+            pieceImageAry[i].setSize(PIECE_SIZE, PIECE_SIZE);
+            pieceImageAry[i].setPosition(PIECE_OFFSET, i + PIECE_OFFSET);
             contextGroup.addActor(pieceImageAry[i]);
         }
     }
