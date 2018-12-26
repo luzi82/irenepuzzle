3c075562cb710863ebf1e5fb46c48bba01c7a281
Luzi82 <luzi82@gmail.com>
2018-12-26 15:59:04 +0800

    fix scroll

--- client/core/src/com/luzi82/irenepuzzle/menuscreen/MenuScreen.java
+++ client/core/src/com/luzi82/irenepuzzle/menuscreen/MenuScreen.java
@@ -5,11 +5,8 @@ import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Cell;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
@@ -20,91 +17,90 @@ import com.luzi82.irenepuzzle.Utils;
 public class MenuScreen extends ScreenAdapter {
 
     // const
-    public static final float BOADER = (float)((1-Math.sqrt(Utils.PHI-1))/2);
+    public static final float BOADER = (float) ((1 - Math.sqrt(Utils.PHI - 1)) / 2);
     public static final float SPACE = BOADER;
-    public static final float INNER_WIDTH = 2*BOADER+SPACE+2;
+    public static final float INNER_WIDTH = 2 * BOADER + SPACE + 2;
 
     Stage stage;
 
     ScrollPane scrollPane;
-    Group tableScale;
     Table table;
+    ImageButton[] ibAry;
 
     Texture texture;
 
     @Override
-    public void show(){
+    public void show() {
         stage = new Stage(new ScreenViewport());
         Gdx.input.setInputProcessor(stage);
 
-//        rootGroup = new Group();
-//        stage.addActor(rootGroup);
+        table = new Table();
+        table.setRound(false);
+        table.setDebug(true);
 
         texture = new Texture(Gdx.files.internal("badlogic.jpg"));
 
-//        table = new Table();
-//        table.setRound(false);
-//        table.setDebug(true);
-//
-//        table.defaults().width(1f).height(1f).space(SPACE);
-//        // table.setBounds(0,0,INNER_WIDTH,2*BOADER+SPACE+2);
-//
-//        texture = new Texture(Gdx.files.internal("badlogic.jpg"));
-//
-//        ImageButton ib0 = new ImageButton(new TextureRegionDrawable(new TextureRegion(texture)));
-//        ib0.setRound(false);
-//        table.add(ib0);
-//
-//        ImageButton ib1 = new ImageButton(new TextureRegionDrawable(new TextureRegion(texture)));
-//        ib1.setRound(false);
-//        table.add(ib1);
-//
-//        table.row();
-//
-//        ImageButton ib2 = new ImageButton(new TextureRegionDrawable(new TextureRegion(texture)));
-//        ib2.setRound(false);
-//        table.add(ib2);
-//
-//        ImageButton ib3 = new ImageButton(new TextureRegionDrawable(new TextureRegion(texture)));
-//        ib3.setRound(false);
-//        table.add(ib3);
+        ibAry = new ImageButton[4];
 
-        Image img = new Image(new TextureRegionDrawable(new TextureRegion(texture)));
-        img.setSize(300,300);
-        //img.setSize(600,600);
+        ibAry[0] = new ImageButton(new TextureRegionDrawable(new TextureRegion(texture)));
+        ibAry[0].setRound(false);
+        table.add(ibAry[0]);
 
-        tableScale = new Group();
-        tableScale.addActor(img);
-        //tableScale.setBounds(0,0,600,600);
+        ibAry[1] = new ImageButton(new TextureRegionDrawable(new TextureRegion(texture)));
+        ibAry[1].setRound(false);
+        table.add(ibAry[1]);
 
-        scrollPane = new ScrollPane(tableScale);
-        scrollPane.setDebug(true,true);
+        table.row();
+
+        ibAry[2] = new ImageButton(new TextureRegionDrawable(new TextureRegion(texture)));
+        ibAry[2].setRound(false);
+        table.add(ibAry[2]);
+
+        ibAry[3] = new ImageButton(new TextureRegionDrawable(new TextureRegion(texture)));
+        ibAry[3].setRound(false);
+        table.add(ibAry[3]);
+
+        scrollPane = new ScrollPane(table);
+        scrollPane.setDebug(true, true);
         scrollPane.layout();
         scrollPane.setScrollingDisabled(true, false);
         scrollPane.setFillParent(true);
         scrollPane.setLayoutEnabled(true);
 
         stage.addActor(scrollPane);
+
+//        stage.addActor(table);
     }
 
     @Override
     public void resize(int width, int height) {
         stage.getViewport().update(width, height, true);
 
-        //tableScale.setScale(width/INNER_WIDTH);
-        tableScale.setScale(2);
-        //tableScale.setBounds(0,0,width,width);
-        //tableScale.setBounds(0,0,300,300);
-        tableScale.setBounds(0,0,600,600);
-        //tableScale.setCullingArea(new Rectangle(0,0,600,600));
+        if (height > width) {
+            float tableHeight = width * (2 + SPACE) / INNER_WIDTH;
+            float top = (height - tableHeight) / (Utils.PHI + 1);
+            if (top < width * SPACE / INNER_WIDTH) {
+                top = width * BOADER / INNER_WIDTH;
+            }
+            table.padLeft(width * BOADER / INNER_WIDTH);
+            table.padRight(width * BOADER / INNER_WIDTH);
+            table.padTop(top);
+            table.padBottom(height - top - tableHeight);
+        } else {
+            table.pad(width * SPACE / INNER_WIDTH);
+        }
+        for (Cell cell : table.getCells()) {
+            cell.space(width * SPACE / INNER_WIDTH);
+            cell.width(width * 1 / INNER_WIDTH);
+            cell.height(width * 1 / INNER_WIDTH);
+        }
 
-        //Rectangle fullRect = new Rectangle(0,0,width,height);
-
-        //Utils.setSize(rootGroup, fullRect, new Vector2(INNER_WIDTH, INNER_WIDTH*height/width));
-        //rootGroup.setBounds(0,0,INNER_WIDTH,INNER_WIDTH*height/width);
+        for (ImageButton ib : ibAry) {
+            ib.getImageCell().width(width * 1 / INNER_WIDTH).height(width * 1 / INNER_WIDTH);
+        }
     }
 
-        @Override
+    @Override
     public void render(float delta) {
         Gdx.gl.glClearColor(0.5f, 0.5f, 0.5f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
